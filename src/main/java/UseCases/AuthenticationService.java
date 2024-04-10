b51b0038df0d0595d0b658b0ccea76aaaabed038
Florian Mueller <florian.mueller@agilent.com>
2024-04-10 11:03:38 +0200

    modified authentication, creates userportfolio on register on MongoDB

--- src/main/java/UseCases/AuthenticationService.java
+++ src/main/java/UseCases/AuthenticationService.java
@@ -5,11 +5,13 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class AuthenticationService {
     private MongoClient mongoClient;
     private MongoDatabase database;
     private MongoCollection<Document> userCollection;
+    private MongoCollection<Document> portfolioCollection;
 
     public AuthenticationService() {
         initializeDatabase();
@@ -20,6 +22,7 @@ public class AuthenticationService {
         mongoClient = MongoClients.create(uri);
         database = mongoClient.getDatabase("BrokeBroker");
         userCollection = database.getCollection("User");
+        portfolioCollection = database.getCollection("Portfolio");
     }
 
     public boolean authenticateUser(String username, String password) {
@@ -28,11 +31,32 @@ public class AuthenticationService {
     }
 
     public void registerUser(String username, String password) {
-        Document existingUser = userCollection.find(new Document("username", username)).first();
-        if (existingUser == null) {
-            Document newUser = new Document("username", username)
-                    .append("password", password); // Passwörter sollten gehasht werden
-            userCollection.insertOne(newUser);
+        if (!userExists(username)) {
+            createUser(username, password);
+            createPortfolioForUser(username);
+        }
+    }
+
+    private boolean userExists(String username) {
+        Document user = userCollection.find(new Document("username", username)).first();
+        return user != null;
+    }
+
+    private void createUser(String username, String password) {
+        // In einem echten Szenario sollte das Passwort gehasht und gesalzen werden
+        Document newUser = new Document("username", username)
+                .append("password", password);
+        userCollection.insertOne(newUser);
+    }
+
+    private void createPortfolioForUser(String username) {
+        Document user = userCollection.find(new Document("username", username)).first();
+        if (user != null) {
+            ObjectId userId = user.getObjectId("_id");
+            Document newPortfolio = new Document("userId", userId.toHexString())
+                    .append("username", username) // Initial leeres Portfolio
+                    .append("stocks", new Document());
+            portfolioCollection.insertOne(newPortfolio);
         }
     }
 }
